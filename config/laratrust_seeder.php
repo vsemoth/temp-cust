d895159cd0f668d94fcaa1276f348b8e4f92c4a9
Lwandile Rozani <vsemoth@gmail.com>
2019-03-21 20:59:10 +0200

    Added Laratrust and configured it

---
+++ config/laratrust_seeder.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'role_structure' => [
+        'superadministrator' => [
+            'users' => 'c,r,u,d',
+            'acl' => 'c,r,u,d',
+            'profile' => 'r,u'
+        ],
+        'administrator' => [
+            'users' => 'c,r,u,d',
+            'profile' => 'r,u'
+        ],
+        'editor' => [
+            'profile' => 'r,u'
+        ],
+        'author' => [
+            'profile' => 'r,u'
+        ],
+        'contributor' => [
+            'profile' => 'r,u'
+        ],
+        'suppoter' => [
+            'profile' => 'r,u'
+        ],
+        'subscriber' => [
+            'profile' => 'r,u'
+        ],
+    ],
+    'permission_structure' => [
+        'cru_user' => [],
+    ],
+    'permissions_map' => [
+        'c' => 'create',
+        'r' => 'read',
+        'u' => 'update',
+        'd' => 'delete'
+    ]
+];
